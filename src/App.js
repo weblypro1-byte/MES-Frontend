c670f1674a164a75d5aecb46c685097b3f3b7509
Umar Akhtar <umarakhtar@Umars-MacBook-Air.local>
2025-10-03 21:40:33 +0500

    update the code, add SEO on index.html. update the app.js and delete some files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import 'antd/dist/reset.css';
-import { message } from "antd";   // ✅ import message here
+
 
 import React from "react";
 
@@ -11,11 +11,10 @@ import './App.scss';
 import Footer from './components/footer';
 import Aboutus from './About/Aboutus';
 import OurCustomer from './components/OurCustomer';
-import Contract from './components/Contract';
+
 import Product from './components/Product';
 import Partners from './components/partners';
-import CoreValuesPage from './Pages/CoreValuesPage';
-import Responsibilty from './Pages/Responsibilty';
+
 import BackButton from './BackButton';
 import  ErrorPage from './ErrorPage'
 
@@ -28,9 +27,8 @@ function App() {
   <Route path="/OurCustomer" element={<div className="page-animation"><OurCustomer /></div>} />
   <Route path="/contact" element={<div className="page-animation"><Contact /></div>} />
   <Route path="/aboutus" element={<div className="page-animation"><Aboutus /></div>} />
-  <Route path="/coreValuesPage" element={<div className="page-animation"><CoreValuesPage /></div>} />
-  <Route path="/responsibilty" element={<div className="page-animation"><Responsibilty /></div>} />
-  <Route path="/Contract" element={<div className="page-animation"><Contract /></div>} />
+
+
   <Route path="/Product" element={<div className="page-animation"><Product /></div>} />
   <Route path="/partners" element={<div className="page-animation"><Partners /></div>} />
   <Route path="*" element={<div className="page-animation"><ErrorPage /></div>} />
